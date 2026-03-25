9a372fa0fc106b1421b532a86ca48729a44de696
Ojas Raverkar <ojas12062006@gmail.com>
2026-03-26 00:01:42 +0530

    news

---
+++ scripts/news_fetcher.py
@@ -0,0 +1,15 @@
+import feedparser
+import re
+
+def get_news(symbol):
+    query = symbol.replace('.NS', '')
+    rss_url = f"https://news.google.com/rss/search?q={query}+stock&hl=en-IN&gl=IN&ceid=IN:en"
+    feed = feedparser.parse(rss_url)
+    articles = []
+    for entry in feed.entries[:10]:
+        articles.append({
+            'title' : entry.title,
+            'link' : entry.link,
+            'published' : entry.published
+        })
+    return articles
