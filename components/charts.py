755d6b0021af46b4b381c60e1980a13c1395d3f4
Ojas Raverkar <ojas12062006@gmail.com>
2026-03-26 00:11:57 +0530

    charts

---
+++ components/charts.py
@@ -0,0 +1,26 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def plot_price_chart(df, title):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.index, y=df['Close'], mode='lines', name='Close'))
+    fig.update_layout(title=title, xaxis_title = 'Date', yaxis_title = 'Price')
+    return fig
+
+def plot_candlesticks(df, title):
+    fig = go.Figure(data=[go.Candlestick(x = df.index,
+                                         open=df['Open'],
+                                         high=df['High'],
+                                         low=df['Low'],
+                                         close=df['Close'])])
+    fig.update_layout(title=title, xaxis_title = 'Date', yaxis_title = 'Price')
+    return fig
+
+def plot_technicals(df, title):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.index, y=df['Close'], mode='lines', name='Close'))
+    for col in ['SMA_50', 'SMA_200']:
+        if col in df.columns:
+            fig.add_trace(go.Scatter(x=df.index, y=df[col], mode='lines', name=col))
+    fig.update_layout(title=title, xaxis_title = 'Date', yaxis_title = 'Price')
+    return fig
